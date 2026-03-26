51f7c7e3701f19dbb8b43141a981454ef57cf130
Iain Earl <iain@itmecho.com>
2026-03-26 13:27:16 +0000

    Create fresh astro project with bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-
-import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkReadingTime],
-    }),
-    tailwind(),
-  ],
-});
+export default defineConfig({});
--- src/lib/cx.ts
+++
@@ -1,6 +0,0 @@
-type ClassOption = string | boolean | undefined;
-
-export const cx = (...classes: ClassOption[]) =>
-  classes.filter((c) => !!c).join(' ');
-
-export default cx;
