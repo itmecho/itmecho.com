91abf7e1150e13c714d3debbea08474a56b947e0
Iain Earl <iain@itmecho.com>
2026-03-28 10:27:59 +0000

    Finish homepage and setup blog collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Pixelify Sans",
+      cssVariable: "--font-pixelated",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "PT Sans",
+      cssVariable: "--font-sans",
+    },
+  ],
+
+  integrations: [mdx()],
+});
---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+import { defineCollection } from "astro:content";
+
+import { glob } from "astro/loaders";
+
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    updatedDate: z.coerce.date(),
+    publishDate: z.coerce.date().optional(),
+    tags: z.string().array().min(1),
+  }),
+});
+
+export const collections = { blog };
