5ea42f4714604800838295565e984f5b2bac019b
Iain Earl <iain@itmecho.com>
2026-05-02 00:57:20 +0100

    Fix sorting

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,9 +1,11 @@
 import { getCollection } from "astro:content";
 
 export async function getBlogPosts(opts: { includeDrafts: boolean }) {
-  const all = await getCollection("blog");
+  const all = (await getCollection("blog")).sort((a, b) =>
+    b.filePath!.localeCompare(a.filePath!),
+  );
   if (!opts.includeDrafts) {
     return all.filter((p) => !!p.data.pubDate);
   }
-  return all.sort((a, b) => b.filePath!.localeCompare(a.filePath!));
+  return all;
 }
