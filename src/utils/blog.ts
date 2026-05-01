102a1b6bf22e31be6915d5674a79cce7ad8782a0
Iain Earl <iain@itmecho.com>
2026-05-02 00:46:54 +0100

    Simplify sorting

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,17 +1,9 @@
 import { getCollection } from "astro:content";
-import { isBefore, isEqual } from "date-fns";
 
 export async function getBlogPosts(opts: { includeDrafts: boolean }) {
   const all = await getCollection("blog");
   if (!opts.includeDrafts) {
     return all.filter((p) => !!p.data.pubDate);
   }
-  return all.sort((a, b) => {
-    const ad = a.data.pubDate ?? Infinity;
-    const bd = b.data.pubDate ?? Infinity;
-    if (isEqual(ad, bd)) {
-      return 0;
-    }
-    return isBefore(ad, bd) ? 1 : -1;
-  });
+  return all.sort((a, b) => b.filePath!.localeCompare(a.filePath!));
 }
